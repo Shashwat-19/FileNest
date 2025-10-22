95e8f4cafd0da9b0b530aa6e3a54efee00e3109e
Shashwat-19 <shashwat1956@gmail.com>
2025-10-22 11:48:03 +0530

    Added: files

--- java Folder/hello.java
+++
@@ -1,7 +0,0 @@
-public class hello {
-    public static void main(String[] args){
-        System.out.println("Hello, how are you?");
-        float a = 10.9f;
-        System.out.println(a);
-    }
-}
